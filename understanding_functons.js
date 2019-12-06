aa48afa35b1000d6beac8ed6e16f1e8724a60a5b
Zayah B <zayah@zayahb.com>
2019-12-06 12:55:38 -0700

    Pushing new changes

---
+++ understanding_functons.js
@@ -0,0 +1,5 @@
+function addTwoNumbers(firstNumber, secondNumber) {
+  console.log("What is " + firstNumber + " + " + secondNumber + "?");
+  return firstNumber + secondNumber;
+}
+console.log(addTwoNumbers(1, 2));
